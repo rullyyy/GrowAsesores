cedd5c4646fbfaf6128ca7e77099ee7852e830f8
waw89 <lcontrerasperaza99@gmail.com>
2024-07-12 03:28:37 -0700

    Avance en responsividad de landing page

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const mobileNav = document.querySelector(".hamburger");
-const navbar = document.querySelector(".menubar");
-
-const toggleNav = () => {
-  navbar.classList.toggle("active");
-  mobileNav.classList.toggle("hamburger-active");
-};
-mobileNav.addEventListener("click", () => toggleNav());
